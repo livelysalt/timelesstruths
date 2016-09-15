cbdb537eba17d32b1f73af34b76f946939d6b1de
livelysalt <livelysalt@users.noreply.github.com>
2016-09-14 20:23:40 -0700

    updated file paths to allow dev branching

--- includes/classes_site.php
+++ includes/classes_site.php
@@ -10,11 +10,11 @@ class SiteSplit {
 		$this->type = $type;
 		$this->title = ucfirst($type);
 		if($type == 'bible' || $type == 'texts' || $type == 'music') {
-			if(file_exists('library/'.$type.'3.php')) {
+			if(stream_resolve_include_path('library/'.$type.'3.php')) {
 				include 'library/'.$type.'3.php';
 			}
 		} else {
-			if(file_exists('www/'.$type.'3.php')) {
+			if(stream_resolve_include_path('www/'.$type.'3.php')) {
 				include 'www/'.$type.'3.php';
 			}
 		}
